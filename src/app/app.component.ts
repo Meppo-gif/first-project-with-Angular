418c0b22bda4a1b99f92a63e653ed327b1f5cdf9
**Meppo-gif <tchuente1999@gmail.com>
2022-02-27 14:40:29 +0100

    27022022 comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // hh : mm : ss
   title = 'time';
+  h:string = ''; //variable representant l'heuire actuelle
+  m:string = ''; //variable reprensentant la minute actuelle
+  s:string = ''; //variable representant la seconde actuelle
+
+  gc:any;
+
+  private afficherHeure()
+  {
+   const heure = new Date(Date.now());
+   this.h = this.formatTime(heure.getHours().toString());
+   this.m = this.formatTime(heure.getMinutes().toString());
+   this.s = this.formatTime(heure.getSeconds().toString());  
+  }
+
+  private mettreAjour()
+  {
+    setInterval( ()=>{
+      this.afficherHeure();
+    },1000)
+  }
+
+  ngOnInit()
+  {
+    this.afficherHeure();
+    this.gc= this.mettreAjour();
+  }
+
+  ngOnDestroy()
+  {
+    clearInterval(this.gc);
+  }
+
+  private formatTime(n:any)
+  {
+    if(n<10) return '0' +n.toString();
+    else return n.toString();
+  }
 }
+
